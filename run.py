e41f3736ad7fc637bd5d0699c3ee06021859ffd9
Lucas Carrafa <lucascarrafa@hotmail.com>
2019-06-03 20:24:44 -0300

    templates e models

--- run.py
+++ run.py
@@ -1,36 +1,4 @@
-from flask import Flask
-
-my_awesome_app = Flask(__name__)
-
-
-@my_awesome_app.route('/')
-def hello_world():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <style>
-    h1 {
-        color: blue;
-        font-family: verdana;
-        font-size: 300%;
-    }
-    p  {
-        color: red;
-        font-family: courier;
-        font-size: 160%;
-    }
-    </style>
-    </head>
-    <body>
-
-    <h1>This is a heading</h1>
-    <p>This is a paragraph.</p>
-
-    </body>
-    </html>
-    """
-
+from app import app
 
 if __name__ == '__main__':
-    my_awesome_app.run()
+    app.run()
